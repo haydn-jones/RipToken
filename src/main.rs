d0788ba3f2e3f090a721e2973740f9b25ee22bd5
Haydn Jones <haydnjonest@gmail.com>
2023-07-08 15:27:27 -0600

    Optimal encoding

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,51 @@
 pub mod utils;
 
-use crate::utils::{read_file, tokenize_selfie};
+use crate::utils::{get_init_vocab, read_file, encode_selfie, decode_selfie, optimal_encode};
+
+use rayon::prelude::*;
 
 fn main() {
-    let lines = read_file("./train_selfies.txt");
-    println!("Number of lines: {}", lines.len());
+    let selfies = read_file("./data/train_selfies.txt");
+    println!("Number of lines: {}", selfies.len());
 
-    let tokens = tokenize_selfie(&lines[1]);
-    println!("Tokens: {:?}", tokens);
+    let mut tokens = get_init_vocab(&selfies);
+    // print vocab
+    for (k, v) in &tokens {
+        println!("{}: {}", k, v);
+    }
+    tokens.insert("[Branch1][C][C][Branch1]".to_string(), tokens.len());
+    tokens.insert("[C][C][Branch1][C][C][Branch1]".to_string(), tokens.len());
+    tokens.insert("[Branch1][C][C]".to_string(), tokens.len());
+    tokens.insert("[C][C]".to_string(), tokens.len());
+
+    println!("Number of unique tokens: {}", tokens.len());
+
+    let encoded = encode_selfie(&selfies[0], &tokens);
+    println!(
+        "Encoded: {:?} -> {}",
+        encoded,
+        decode_selfie(&encoded, &tokens)
+    );
+
+    let opt_encode = optimal_encode(&selfies[0], &tokens).unwrap();
+    println!(
+        "Optimal encoded: {:?} -> {}",
+        opt_encode,
+        decode_selfie(&opt_encode, &tokens)
+    );
+
+    // time how long it takes to encode all selfies
+    let start = std::time::Instant::now();
+    for selfie in &selfies {
+        let _ = encode_selfie(selfie, &tokens);
+    }
+    println!("Time to encode all selfies: {:?}", start.elapsed());
+
+    // time how long it takes to optimally encode all selfies
+    let start = std::time::Instant::now();
+    let _encs: Vec<Vec<usize>> = selfies.par_iter().map(|selfie| optimal_encode(selfie, &tokens).unwrap()).collect();
+    println!(
+        "Time to optimally encode all selfies: {:?}",
+        start.elapsed()
+    );
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -12,21 +13,101 @@ pub fn read_file(filename: &str) -> Vec<String> {
     lines
 }
 
-// This assumes the selfie is valid, please don't give poor little Rusty invalid selfies
-// he's just a little guy
-pub fn tokenize_selfie(selfie: &str) -> Vec<String> {
+pub fn split_selfie_opt(selfie: &str) -> Vec<(usize, usize)> {
     let mut tokens = Vec::new();
 
     let mut token_start = 0;
     for (i, c) in selfie.char_indices() {
         match c {
             ']' => {
-                tokens.push(selfie[token_start..i+1].to_string());
-                token_start = i+1;
-            },
+                tokens.push((token_start, i + 1));
+                token_start = i + 1;
+            }
             _ => continue,
         }
     }
 
     tokens
+}
+
+// This assumes the selfie is valid, please don't give poor little Rusty invalid selfies
+// he's just a little guy
+pub fn split_selfie(selfie: &str) -> Vec<String> {
+    let mut tokens = Vec::new();
+
+    let mut token_start = 0;
+    for (i, c) in selfie.char_indices() {
+        match c {
+            ']' => {
+                tokens.push(selfie[token_start..i + 1].to_string());
+                token_start = i + 1;
+            }
+            _ => continue,
+        }
+    }
+
+    tokens
+}
+
+pub fn get_init_vocab(selfies: &Vec<String>) -> HashMap<String, usize> {
+    // get set of unique tokens
+    let mut vocab = HashMap::new();
+    for selfie in selfies {
+        let tokens = split_selfie(selfie);
+        for token in tokens {
+            let len = vocab.len();
+            vocab.entry(token).or_insert(len);
+        }
+    }
+
+    vocab
+}
+
+pub fn decode_selfie(encoded: &Vec<usize>, vocab: &HashMap<String, usize>) -> String {
+    // reverse vocab
+    let rev_vocab: HashMap<usize, String> = vocab.iter().map(|(k, v)| (*v, k.clone())).collect();
+    let mut decoded = String::new();
+
+    for token in encoded {
+        decoded.push_str(&rev_vocab[token]);
+    }
+    decoded
+}
+
+pub fn optimal_encode(
+    selfie: &str,
+    vocab: &HashMap<String, usize>,
+) -> Result<Vec<usize>, &'static str> {
+    let token_indices: Vec<(usize, usize)> = split_selfie_opt(selfie);
+    let mut encodings: Vec<Vec<usize>> = vec![vec![]; token_indices.len() + 1];
+
+    // Dynamic programming approach to find optimal encoding
+    // i.e. the encoding with the fewest tokens
+    for i in 0..token_indices.len() {
+        for j in 0..=i {
+            if !encodings[i + 1].is_empty() && (encodings[j].len() + 1) >= encodings[i + 1].len() {
+                continue;
+            }
+
+            let substring = &selfie[token_indices[j].0..token_indices[i].1];
+
+            if let Some(value) = vocab.get(substring) {
+                encodings[i + 1] = encodings[j].clone();
+                encodings[i + 1].push(*value);
+            }
+        }
+    }
+
+    encodings.last().cloned().ok_or("Cannot encode string with given vocab")
+}
+
+pub fn encode_selfie(selfie: &str, vocab: &HashMap<String, usize>) -> Vec<usize> {
+    let mut encoded = Vec::new();
+
+    let tokens = split_selfie(selfie);
+    for token in tokens {
+        encoded.push(vocab[&token]);
+    }
+
+    encoded
 }
